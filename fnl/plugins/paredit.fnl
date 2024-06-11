b991186d5083a46e27f1df30a82ba85776c3d698
yanchim <yanchi.x@foxmail.com>
2024-06-11 21:12:00 +0800

    Disable autopair in lisp, add paredit keybinding

--- fnl/plugins/paredit.fnl
+++ fnl/plugins/paredit.fnl
@@ -1,9 +1,32 @@
+(local lisp-ft [:clojure :fennel :lisp :scheme])
+
 [{1 :julienvincent/nvim-paredit
   :lazy true
-  :ft [:clojure :fennel]
+  :ft lisp-ft
   :config (fn []
             (let [paredit (require :nvim-paredit)]
-              (paredit.setup)))}
+              (paredit.setup
+                {:keys
+                 {:<M-H> [paredit.api.slurp_backwards "Slurp backwards"]
+                  :<M-J> [paredit.api.barf_backwards "Barf backwards"]
+                  :<M-K> [paredit.api.barf_forwards "Barf forwards"]
+                  :<M-L> [paredit.api.slurp_forwards "Slurp forwards"]
+
+                  :<localleader>9
+                  [(fn []
+                     (paredit.cursor.place_cursor
+                       (paredit.wrap.wrap_element_under_cursor "( " ")")
+                       {:placement :inner_start
+                        :mode :insert}))
+                   "Wrap element insert head"]
+
+                  :<localleader>0
+                  [(fn []
+                     (paredit.cursor.place_cursor
+                       (paredit.wrap.wrap_element_under_cursor "(" ")")
+                       {:placement :inner_end
+                        :mode :insert}))
+                   "Wrap element insert tail"]}})))}
 
  {1 :julienvincent/nvim-paredit-fennel
   :dependencies [:julienvincent/nvim-paredit]
@@ -21,4 +44,4 @@
                   autopairs-cmp (require :nvim-autopairs.completion.cmp)
                   cmp (require :cmp) ]
               (cmp.event:on :confirm_done (autopairs-cmp.on_confirm_done))
-              (autopairs.setup {:check_ts true})))}]
+              (autopairs.setup {:disable_filetype lisp-ft})))}]
--- lua/plugins/paredit.lua
+++ lua/plugins/paredit.lua
@@ -1,17 +1,24 @@
 -- [nfnl] Compiled from fnl/plugins/paredit.fnl by https://github.com/Olical/nfnl, do not edit.
+local lisp_ft = {"clojure", "fennel", "lisp", "scheme"}
 local function _1_()
   local paredit = require("nvim-paredit")
-  return paredit.setup()
+  local function _2_()
+    return paredit.cursor.place_cursor(paredit.wrap.wrap_element_under_cursor("( ", ")"), {placement = "inner_start", mode = "insert"})
+  end
+  local function _3_()
+    return paredit.cursor.place_cursor(paredit.wrap.wrap_element_under_cursor("(", ")"), {placement = "inner_end", mode = "insert"})
+  end
+  return paredit.setup({keys = {["<M-H>"] = {paredit.api.slurp_backwards, "Slurp backwards"}, ["<M-J>"] = {paredit.api.barf_backwards, "Barf backwards"}, ["<M-K>"] = {paredit.api.barf_forwards, "Barf forwards"}, ["<M-L>"] = {paredit.api.slurp_forwards, "Slurp forwards"}, ["<localleader>9"] = {_2_, "Wrap element insert head"}, ["<localleader>0"] = {_3_, "Wrap element insert tail"}}})
 end
-local function _2_()
+local function _4_()
   local paredit_fnl = require("nvim-paredit-fennel")
   return paredit_fnl.setup()
 end
-local function _3_()
+local function _5_()
   local autopairs = require("nvim-autopairs")
   local autopairs_cmp = require("nvim-autopairs.completion.cmp")
   local cmp = require("cmp")
   do end (cmp.event):on("confirm_done", autopairs_cmp.on_confirm_done())
-  return autopairs.setup({check_ts = true})
+  return autopairs.setup({disable_filetype = lisp_ft})
 end
-return {{"julienvincent/nvim-paredit", lazy = true, ft = {"clojure", "fennel"}, config = _1_}, {"julienvincent/nvim-paredit-fennel", dependencies = {"julienvincent/nvim-paredit"}, lazy = true, ft = {"fennel"}, config = _2_}, {"windwp/nvim-autopairs", event = "InsertEnter", dependencies = {"hrsh7th/nvim-cmp"}, config = _3_}}
+return {{"julienvincent/nvim-paredit", lazy = true, ft = lisp_ft, config = _1_}, {"julienvincent/nvim-paredit-fennel", dependencies = {"julienvincent/nvim-paredit"}, lazy = true, ft = {"fennel"}, config = _4_}, {"windwp/nvim-autopairs", event = "InsertEnter", dependencies = {"hrsh7th/nvim-cmp"}, config = _5_}}
